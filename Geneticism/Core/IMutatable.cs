91f77f200ecc7e91d08c9507c90d5cf390932d45
Matthew Gianino <mgianino@r2integrated.com>
2018-03-26 12:13:15 -0400

    Add project files.

---
+++ Geneticism/Core/IMutatable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geneticism.Core
+{
+    public interface IMutatable<T>
+    {
+        T Genome { get; set; }
+        void Mutate();
+    }
+}
